049a8953b04e4f472a527a7caa8c134ad325bdc9
Babithkp <babithkullachetti44@gmail.com>
2025-02-20 19:39:12 +0530

    Initial commit

---
+++ test/cdk-lamda-dynamodb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLamdaDynamodb from '../lib/cdk-lamda-dynamodb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-lamda-dynamodb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLamdaDynamodb.CdkLamdaDynamodbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
